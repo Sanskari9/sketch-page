3c2b16f609097ca51c2edf53f16ad4a8781a098b
rajmal varma <rajmalvarma9@gmail.com>
2023-04-21 11:01:09 +0530

    main section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,337 @@
-import React from 'react'
-
-const Footer = () => {
+import React from "react";
+import {IoDiamond } from "react-icons/io5"
+import {AiFillTwitterCircle, AiFillInstagram , AiFillYoutube} from "react-icons/ai"
+import {BsMastodon} from "react-icons/bs"
+function Footer() {
   return (
-    <div>
+                      // Footer Section Start
+    <footer className="relative z-10 bg-white pt-20 pb-10 lg:pt-[120px] lg:pb-20 ">
+         <div className="container mx-auto">
+    <div className="mx-[10rem] justify-between flex  flex-wrap ">
+          <div className="w-full px-4 sm:w-1/2 lg:w-2/12">
+            <div className="mb-10 w-full">
+              <h4 className="text-dark text-lg font-semibold">Product</h4>
+              <ul>
+                <li>
+                  <a
+                    href="#!"
+                    className="text-body-color hover:text-primary  inline-block text-base leading-loose"
+                  >
+                    What's New
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#!"
+                    className="text-body-color hover:text-primary  inline-block text-base leading-loose"
+                  >
+                    Design
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#!"
+                    className="text-body-color hover:text-primary  inline-block text-base leading-loose"
+                  >
+                   Collaboration
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#!"
+                    className="text-body-color hover:text-primary  inline-block text-base leading-loose"
+                  >
+                   Prototyping
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#!"
+                    className="text-body-color hover:text-primary  inline-block text-base leading-loose"
+                  >
+                   Developer Handoff
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#!"
+                    className="text-body-color hover:text-primary  inline-block text-base leading-loose"
+                  >
+                   Apps
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#!"
+                    className="text-body-color hover:text-primary  inline-block text-base leading-loose"
+                  >
+                 Extensions
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#!"
+                    className="text-body-color hover:text-primary  inline-block text-base leading-loose"
+                  >
+                 All Features
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    href="#!"
+                    className="text-body-color hover:text-primary  inline-block text-base leading-loose"
+                  >
+                  Pricing
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+                  
+          <div className="w-full px-4 sm:w-1/2 lg:w-2/12">
+            <div className="mb-10 w-full">
+              <h4 className="text-dark  text-lg font-semibold">Why Sketch</h4>
+              <ul>
+                <li>
+                  <a
+                    href="#!"
+                    className="text-body-color hover:text-primar inline-block text-base leading-loose"
+                  >
+                    Switch to Sketch
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#!"
+                    className="text-body-color hover:text-primary inline-block text-base leading-loose"
+                  >
+                    Sketch vs. Figma
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#!"
+                    className="text-body-color hover:text-primary inline-block text-base leading-loose"
+                  >
+                   Sketch vs. Adobe XD
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#!"
+                    className="text-body-color hover:text-primar inline-block text-base leading-loose"
+                  >
+                    Free for Education
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+
+          <div Name="w-full px-4 sm:w-1/2 lg:w-2/12">
+            <div className="mb-10 w-full">
+              <h4 className="text-dark  text-lg font-semibold">Learn</h4>
+              <ul>
+                <li>
+                  <a
+                    href="#!"
+                    className="text-body-color hover:text-primary inline-block text-base leading-loose"
+                  >
+                Blog
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#!"
+                    className="text-body-color hover:text-primary inline-block text-base leading-loose"
+                  >
+                    Course: Sketch 101
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#!"
+                    className="text-body-color hover:text-primary inline-block text-base leading-loose"
+                  >
+                     Course: Sketch 102
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#!"
+                    className="text-body-color hover:text-primary inline-block text-base leading-loose"
+                  >
+                   Documentation
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#!"
+                    className="text-body-color hover:text-primary inline-block text-base leading-loose"
+                  >
+               Forum & Events
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#!"
+                    className="text-body-color hover:text-primary inline-block text-base leading-loose"
+                  >
+                  Newsletter
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#!"
+                    className="text-body-color hover:text-primary inline-block text-base leading-loose"
+                  >
+               Developer Plateform
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          
+          <div className="w-full px-4 sm:w-1/2 lg:w-2/12">
+            <div className="mb-10 w-full">
+              <h4 className="text-dark  text-lg font-semibold">Support</h4>
+              <ul>
+                <li>
+                  <a
+                    href="#!"
+                    className="text-body-color hover:text-primary inline-block text-base leading-loose"
+                  >
+                     Sketch Help
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#!"
+                    className="text-body-color hover:text-primary inline-block text-base leading-loose"
+                  >
+                    Contact Us
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#!"
+                    className="text-body-color hover:text-primary inline-block text-base leading-loose"
+                  >
+                  Manage Licence
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#!"
+                    className="text-body-color hover:text-primary inline-block text-base leading-loose"
+                  >
+                    Service Status
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+       
+          <div classNameName="w-full px-4 sm:w-1/2 lg:w-2/12">
+            <div className="mb-10 w-full">
+              <h4 className="text-dark  text-lg font-semibold">Company</h4>
+              <ul>
+                <li>
+                  <a
+                    href="#!"
+                    className="text-body-color hover:text-primary inline-block text-base leading-loose"
+                  >
+                   About Us
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#!"
+                    className="text-body-color hover:text-primary inline-block text-base leading-loose"
+                  >
+                   Careers
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#!"
+                    className="text-body-color hover:text-primary inline-block text-base leading-loose"
+                  >
+                   Security
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#!"
+                    className="text-body-color hover:text-primary inline-block text-base leading-loose"
+                  >
+                   Terms & Policies
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#!"
+                    className="text-body-color hover:text-primary inline-block text-base leading-loose"
+                  >
+                 Cookie Preferences
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+ 
+          </div>
+          </div>
+
+
+          <div className="w-full flex flex-wrap justify-between px-[10rem] ml-4">
+        
+        
+             <IoDiamond/>
+           
+          <p className="text-body-color text-base ml-10">© 2023 Sketch B.V</p>
+         
+            
+          <div className="mb-6 flex items-center">
+            <a
+              href="#!"
+              className="text-dark hover:bg-black hover:border-slate-600 mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-[#E5E5E5] hover:text-white sm:mr-4 lg:mr-3 xl:mr-4"
+            >
+           <AiFillTwitterCircle/>
+             
+            </a>
+            <a
+              href="#!"
+              className="text-dark hover:bg-black hover:border-slate-600 mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-[#E5E5E5] hover:text-white sm:mr-4 lg:mr-3 xl:mr-4"
+            >
+             <AiFillInstagram />   
+            </a>
+            <a
+              href="#!"
+              className="text-dark hover:bg-black hover:border-slate-600 mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-[#E5E5E5] hover:text-white sm:mr-4 lg:mr-3 xl:mr-4"
+            >
+               <AiFillYoutube/>       
+            </a>
+            <a
+              href="#!"
+              className="text-dark hover:bg-black hover:border-slate-600 mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-[#E5E5E5] hover:text-white sm:mr-4 lg:mr-3 xl:mr-4"
+            >
+             <BsMastodon/>
+            </a>
+          </div>
+          
+        </div>
     
-    </div>
-  )
+
+
+    </footer>
+                          // Footer Section End 
+    
+  );
 }
 
-export default Footer
+export default Footer;
