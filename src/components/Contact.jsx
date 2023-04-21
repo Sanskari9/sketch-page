b7b374ff0a12b9cd3c4bf74639469d9c9449cddb
Santosh <santoshkushwah01012001@gmail.com>
2023-04-21 14:40:24 +0530

    gsap animation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div className="max-w-[1180px] m-auto flex items-center justify-between">
-      <div >
+    <div className="max-w-[1180px] m-auto flex md:flex-row flex-col items-center justify-between p-4">
+      <div className=" my-4 text-center md:text-start" >
         <h1 className="font-bold">Incredible design, right in your inbox</h1>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod, quas! Lorem ipsum dolor sit amet.</p>
         </div>
         <div>
-            <form>
-                <input type="email" placeholder="Email address" className="border m-2 p-2 rounded-lg bg-slate-200" />
-                <button className="text-white bg-gray-400 px-2 py-2 rounded-lg hover:bg-black">Try it out</button>
+            <form className='grid grid-cols-3'>
+                <input type="email" placeholder="Email address" className=" col-span-2 border my-2 p-2 rounded-lg bg-slate-200" />
+                <button className="text-white bg-gray-400 my-2 p-2 mx-1 rounded-lg hover:bg-black">Try it out</button>
+                <span className='col-span-3 flex'>
+                <input type="checkbox"/> <p className="mx-2 text-gray-400">I agree to receive marketing emails from Sketch.</p>
+                </span>
+                
             </form>
         </div>
     </div>
